02f7e63d0bd02b4e4b022415ec8fc81a989e6b9f
hyojhand <sonjw5128@gmail.com>
2022-06-16 02:48:36 +0900

    [Add] MemberApiController

--- jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,2 +1,48 @@
-package jpabook.jpashop.api;public class MemberApiController {
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members") // api에 엔티티가 종속적이다
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members") // api 스펙에 맞춰서 개발이 가능 (별도의 객체,Dto를 받아서 사용)
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 }
